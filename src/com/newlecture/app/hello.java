2ee8f0b63d8db9fa80553b2a2798de7421d40b0e
NamKiHyeok <carpediem7490@gmail.com>
2023-06-07 11:30:17 +0900

    23.06.07 firstcommit

---
+++ src/com/newlecture/app/hello.java
@@ -0,0 +1,19 @@
+package com.newlecture.app;
+
+class hello{
+    public static void main (String[] args){
+		int kor1, kor2, kor3;
+	    int total;
+	    float avg;
+	
+	    kor2 = 60;
+	    kor3 = 80;
+	    kor1 = 50;
+	    total = kor1 + kor2 + kor3;
+	    avg = total / 3;
+	    int x = 3276812;
+	
+		System.out.printf("total is %d\n", total);
+		System.out.printf("avg is %f\n", avg);
+    }
+}
