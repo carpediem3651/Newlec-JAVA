a21a7ab9ecb86ab3cb4e7e9796c1be12f34f4587
NamKiHyeok <carpediem7490@gmail.com>
2023-06-08 17:02:44 +0900

    2023.06.08 과제 해결

---
+++ src/com/newlecture/app/ex2/cat.java
@@ -0,0 +1,12 @@
+package com.newlecture.app.ex2;
+
+public class cat {
+
+	public static void main(String[] args) {
+		System.out.print("\\  /\\\n");
+		System.out.print(" ) (')\n");
+		System.out.print(")( / )\n");
+		System.out.print(" \\(___)|");
+	}
+
+}
