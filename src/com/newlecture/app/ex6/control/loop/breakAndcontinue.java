bb6aa1a43ca8fbbecb3102d1fd49b78c84b5a5f8
NamKiHyeok <carpediem7490@gmail.com>
2023-06-14 15:36:16 +0900

    브레이크 컨티뉴 실습

--- src/com/newlecture/app/ex6/control/loop/breakAndcontinue.java
+++ src/com/newlecture/app/ex6/control/loop/breakAndcontinue.java
@@ -12,22 +12,24 @@ public class breakAndcontinue {
 		FileInputStream fis = new FileInputStream("res/exam.list");
 		Scanner fscan = new Scanner(fis);
 		
+		int index = 0; // while문 횟수 제한을 위해 인덱스 변수를 선언한다.
 		
-//		while(fscan.hasNext()) { 
+		while(fscan.hasNext()) { 
+			String word = fscan.next(); //next는 문자열의 공백을 기준으로 한 단어 한 문자를 읽는다.
+			int kor = Integer.parseInt(word);//word의 데이터 값이 문자열이기 때문에 String -> Int로 바꾸는 Integer.parseInt()를 사용한다.
+				if(index==5)
+					break;
+			index++;
+			System.out.printf("%d,",kor);
+		}
+		
+//		for(int i=0; i<10; i++) {
 //			String word = fscan.next(); //next는 문자열의 공백을 기준으로 한 단어 한 문자를 읽는다.
 //			int kor = Integer.parseInt(word); //word의 데이터 값이 문자열이기 때문에 String -> Int로 바꾸는 Integer.parseInt()를 사용한다.
-//			 if(kor==5)
+//			 if(i==5)
 //				 break;
 //			System.out.printf("%d,",kor);
 //		}
-		
-		for(int i=0; i<10; i++) {
-			String word = fscan.next(); //next는 문자열의 공백을 기준으로 한 단어 한 문자를 읽는다.
-			int kor = Integer.parseInt(word); //word의 데이터 값이 문자열이기 때문에 String -> Int로 바꾸는 Integer.parseInt()를 사용한다.
-			 if(i==5)
-				 break;
-			System.out.printf("%d,",kor);
-		}
 			
 		
 		fscan.close();
