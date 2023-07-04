e9c86cf0f13d64870f5ef8b08df1dbc4a7e5ce5f
NamKiHyeok <carpediem7490@gmail.com>
2023-07-04 14:15:20 +0900

    2023.07.04 객체지향(객체와개체/개체의 속성/static메소드와 인스턴스 메소드)/ 생성자)

---
+++ src/ex11/oop/basic/Exam.java
@@ -0,0 +1,26 @@
+package ex11.oop.basic;
+
+public class Exam {
+	int kor;
+	int eng;
+	int math;
+	
+	public Exam() {
+		kor=100;
+		eng=80;
+		math=60;
+	}
+	
+	public void print() {
+		
+	}
+
+	public void input() {
+
+	}
+	
+	public void save() {
+		
+	}
+	
+}
---
+++ src/ex11/oop/basic/Program.java
@@ -0,0 +1,12 @@
+package ex11.oop.basic;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Exam exam =  new Exam();
+//		exam.print();
+		
+		System.out.println(exam.math);
+		
+	}
+}
